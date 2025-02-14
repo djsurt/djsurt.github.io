2ff767c51754d79c23a4f1b62a14338294c8c1bb
Dhananjay Surti <djsurti3003@gmail.com>
2025-02-14 15:41:33 -0800

    Add personal projects

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,20 @@ import ProjectTile from "@/components/ProjectTile";
 
 const projects = [
   {
-    title: "Next.js Blog",
-    description: "A minimal blog built with Next.js and Tailwind CSS.",
+    title: "Github repo summarizer using RAG (WIP)",
+    description: "Currently working on an appliction that summarizes and answers specific content in Github repos",
     imageUrl: "/images/nextjs-blog.png",
-    githubLink: "https://github.com/yourusername/nextjs-blog",
+    githubLink: "https://github.com/djsurt/planet",
   },
   {
-    title: "AI Image Classifier",
-    description: "A deep learning model that classifies images with high accuracy.",
+    title: "Speech Therapy Application",
+    description: "An application to help ease communication and interactions between speech therapists and students",
     imageUrl: "/images/ai-classifier.png",
-    githubLink: "https://github.com/yourusername/ai-classifier",
+    githubLink: "https://github.com/WyldTaegr/confident-voices",
   },
   {
-    title: "Task Manager App",
-    description: "A simple task manager built using React and Firebase.",
+    title: "Smart Lamp (LAMPI)",
+    description: "My first Internet of Things project. It's A lamp that can change hue, saturation, and brightness by communicating with a website, iPhone or an Apple Watch!",
     imageUrl: "/images/task-manager.png",
     githubLink: "https://github.com/yourusername/task-manager",
   },
@@ -35,6 +35,19 @@ export default function Home() {
 
       <section className="bg-blue-500 text-white p-8 text-center">
         <p className="text-4xl"> Projects that I am proud of </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {
+            projects.map((project, index) => (
+              <ProjectTile
+              key={index}
+              title={project.title}
+              description={project.description}
+              imageUrl={project.imageUrl}
+              githubLink={project.githubLink}
+              />
+            ))
+          }
+        </div>
       </section>
       
     </div>
